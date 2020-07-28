c0551ae368c21af7081aa0d8c1a9fc872edeb2e6
mhans19 <mdhansen19@gmail.com>
2020-07-27 21:32:42 -0600

    added through association to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,7 +15,9 @@ router.get('/', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['id', 'tag_name']
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'tags'
       }
     ]
   })
@@ -41,7 +43,9 @@ router.get('/:id', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['id', 'tag_name']
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'tags'
       }
     ]
   })
